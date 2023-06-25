7316ff7bf73d7a5ad3254b102147dc65a3e2aca2
Kirill Igumenshchev <kirilligum@gmail.com>
2023-06-25 20:48:42 +0900

    update

--- apps/council-ui/src/config/council.config.ts
+++ apps/council-ui/src/config/council.config.ts
@@ -1,11 +1,13 @@
 import { CouncilConfig } from "src/config/CouncilConfig";
 import { localhostCouncilConfig } from "src/config/localhost";
 
-// export type SupportedChainId = 1 | 5 | 31337;
+// export type SupportedChainId = 1 | 5 | 100 | 31337;
+// export type SupportedChainId = 100 | 31337;
 export type SupportedChainId = 31337;
 
 export const councilConfigs: Record<SupportedChainId, CouncilConfig> = {
   // 1: mainnetCouncilConfig,
   // 5: goerliCouncilConfig,
+  // 100: gnosisCouncilConfig,
   31337: localhostCouncilConfig,
 };
