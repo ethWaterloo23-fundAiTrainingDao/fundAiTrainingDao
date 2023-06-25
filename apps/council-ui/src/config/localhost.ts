3067d27e20d41220075adcdef54579c9f4cc8bab
Kirill Igumenshchev <kirilligum@gmail.com>
2023-06-25 22:08:03 +0900

    update

--- apps/council-ui/src/config/localhost.ts
+++ apps/council-ui/src/config/localhost.ts
@@ -8,18 +8,18 @@ export const localhostCouncilConfig: CouncilConfig = {
   },
   coreVoting: {
     name: "Core Voting",
-     address: "0xe7f1725e7734ce288f8367e1bb143e90bb3f0512",
-     descriptionURL: "https://moreinfo.com",
+    address: "0xe7f1725e7734ce288f8367e1bb143e90bb3f0512",
+    descriptionURL: "https://moreinfo.com",
     vaults: [
       {
-        name: "Locking Vault",
-         address: "0xcf7ed3acca5a467e9e704c703e8d87f634fb0fc9",
+        name: "Data gathering",
+        address: "0xcf7ed3acca5a467e9e704c703e8d87f634fb0fc9",
         type: "LockingVault",
         descriptionURL: "https://moreinfo.com",
       },
       {
-        name: "Fund Vault",
-        address: "0xa51c1fc2f0d1a1b8494ed1fe312d7c3a78ed91c0",
+        name: "Vector Embedings",
+        address: "0xc6ba8c3233ecf65b761049ef63466945c362edd2",
         type: "FundVault",
         descriptionURL: "https://moreinfo.com",
       },
@@ -35,7 +35,7 @@ export const localhostCouncilConfig: CouncilConfig = {
           "As of today (April 28, 2022) there has only been one delegate (myself) who has proven their membership to the GSC on-chain. In roughly five days I will be able to pass votes by myself with no further approval because the current quroum threshold on the GSC is one. I believe that no one person should be able to govern the GSC by themselves, and thus I am proposing to effectively lock the GSC until two other delegates join the GSC. Three was chosen as a starting point for one reason, to break a tie. As more delegates join, I'm sure this value will gradually increase but for the time being, a threshold of three will be adequate to ensure some level of participation.",
       },
     },
-   },
+  },
 
   // gscVoting: {
   //   name: "GSC",
--- packages/council-cli/src/utils/chains.ts
+++ packages/council-cli/src/utils/chains.ts
@@ -6,8 +6,75 @@ import {
   mainnet,
   optimism,
   polygon,
+  polygonMumbai,
   sepolia,
 } from "viem/chains";
+
+const xdcMainnet: any = {
+  id: 50,
+  name: "XDC Mainnet",
+  network: "xdcmainnet",
+  nativeCurrency: {
+    decimals: 18,
+    name: "XDC",
+    symbol: "XDC",
+  },
+  rpcUrls: {
+    default: "https://erpc.xinfin.network",
+    public: "https://erpc.xinfin.network",
+  },
+  blockExplorers: {
+    default: {
+      name: "XDC Apothem Explorer",
+      url: "https://explorer.xinfin.network/",
+    },
+  },
+  testnet: false,
+};
+
+const xdcApothem: any = {
+  id: 51,
+  name: "XDC Apothem Network",
+  network: "apothem",
+  nativeCurrency: {
+    decimals: 18,
+    name: "Test XDC",
+    symbol: "tXDC",
+  },
+  rpcUrls: {
+    default: "https://erpc.apothem.network",
+    public: "https://erpc.apothem.network",
+  },
+  blockExplorers: {
+    default: {
+      name: "XDC Apothem Explorer",
+      url: "https://explorer.apothem.network/",
+    },
+  },
+  testnet: true,
+};
+
+const polygonMumbaiCustom: any = {
+  id: 80001,
+  name: "Mumbai",
+  network: "mumbai",
+  nativeCurrency: {
+    decimals: 18,
+    name: "MATIC",
+    symbol: "MATIC",
+  },
+  rpcUrls: {
+    default: "https://rpc-mumbai.matic.today",
+    public: "https://rpc-mumbai.matic.today",
+  },
+  blockExplorers: {
+    default: {
+      name: "Polygon PoS Chain Testnet Explorer",
+      url: "https://mumbai.polygonscan.com/",
+    },
+  },
+  testnet: true,
+};
 
 export const supportedChains = {
   localhost: hardhat,
@@ -17,7 +84,11 @@ export const supportedChains = {
   optimism,
   arbitrum,
   polygon,
+  polygonMumbai,
+  polygonMumbaiCustom,
   gnosisChiado,
+  xdcMainnet,
+  xdcApothem,
 };
 
 export type SupportedChain = keyof typeof supportedChains;
